44a4061273a557d4e10c3c734a92546a54044dea
Johannes Kliemann <kliemann@componolit.com>
2018-01-15 09:33:28 +0100

    add socket_t type

--- src/rilproxy.h
+++ src/rilproxy.h
@@ -7,17 +7,42 @@ enum
     RILPROXY_BUFFER_SIZE = 3000
 };
 
+typedef struct {
+    uint8_t source[6];
+    uint8_t destination[6];
+    uint16_t type;
+} __attribute__((packed)) ethernet_frame_t;
+
+typedef union {
+    ethernet_frame_t frame;
+} packet_meta_data_t;
+
+typedef enum {
+    RAW,
+    UDP,
+    UNIX
+} socket_type_t;
+
+typedef struct {
+    socket_type_t type;
+    int socket;
+    packet_meta_data_t meta;
+} socket_t;
+
 int socket_copy (int source_fd, int dest_fd, const char *local, const char *remote);
 
-int udp_socket (const char *host, unsigned short port);
-int unix_client_socket (const char *socket_path);
-int unix_server_socket (const char *socket_path, const char *user);
-int raw_ethernet_socket(const char *interface_name);
+socket_t *udp_socket (const char *host, unsigned short port);
+socket_t *unix_client_socket (const char *socket_path);
+socket_t *unix_server_socket (const char *socket_path, const char *user);
+socket_t *raw_ethernet_socket(const char *interface_name);
 int get_uid (const char *username);
 int get_gid (const char *username);
-int send_control_message (int fd, uint32_t message_type);
-void wait_control_message (int fd, uint32_t message_type);
-void proxy (int local_fd, int remote_fd);
+int send_control_message (socket_t* sock, uint32_t message_type);
+void wait_control_message (socket_t* sock, uint32_t message_type);
+void proxy (socket_t *local_fd, socket_t *remote_fd);
+
+ssize_t s_write(socket_t *, const void*, size_t);
+ssize_t s_read(socket_t *, void*, size_t);
 
 typedef struct
 {
